ebad970bdc6ea895677523c5758f784d6b5b29e5
G808 <hucciallstars@gmail.com>
2023-01-03 20:26:07 +0200

    Edited NotFound comp

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="error_page">
+    <div className='error_page'>
       <div
-        className="hero bg-image"
+        className='hero bg-image'
         style={{
-          backgroundImage: `url(${process.env.PUBLIC_URL + "img/1.jpg"})`,
+          backgroundImage: `url(${'img/3.jpg'})`,
         }}
       >
-        <div className="content">
-          <h1 data-aos="fade-up" data-aos-duration="1200">
+        <div className='content'>
+          <h1 data-aos='fade-up' data-aos-duration='1200'>
             404!
           </h1>
-          <p data-aos="fade-up" data-aos-duration="1200" data-aos-delay="50">
-            Ooops! You weren't supposed to see this.
+          <p data-aos='fade-up' data-aos-duration='1200' data-aos-delay='50'>
+            Ooops! Page not found
           </p>
 
           <div
-            className="button"
-            data-aos="fade-up"
-            data-aos-duration="1200"
-            data-aos-delay="100"
+            className='button'
+            data-aos='fade-up'
+            data-aos-duration='1200'
+            data-aos-delay='100'
           >
-            <Link to="/">BACK TO HOME</Link>
+            <Link to='/'>BACK TO HOME</Link>
           </div>
           {/* End purchase_button */}
         </div>
