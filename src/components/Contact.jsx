92b1633cd98e3fd55780fc3a202e941b327b3cb7
G808 <hucciallstars@gmail.com>
2023-03-05 23:25:10 +0200

    Improved Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import emailjs from 'emailjs-com';
 import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 
 const Contact = () => {
   const {
@@ -8,20 +9,27 @@ const Contact = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (formData, e) => {
-    e.preventDefault();
-    emailjs
-      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+  const [successMessage, setSuccessMessage] = useState('');
 
+  const onSubmit = async (formData, e) => {
+    e.preventDefault();
+
+    try {
+      const result = await emailjs.sendForm(
+        'service_y0i6p7j',
+        'template_b22amot',
+        e.target,
+        'wHKDnOuPASssRRaFd'
+      );
+
+      console.log(result.text);
+      setSuccessMessage('Message sent successfully!');
+      e.target.reset();
+      setTimeout(() => setSuccessMessage(''), 5000); // Clear success message after 5 seconds
+    } catch (error) {
+      console.log(error.text);
+    }
+  };
   return (
     <>
       <form className='contactform' onSubmit={handleSubmit(onSubmit)}>
@@ -101,9 +109,14 @@ const Contact = () => {
               <span className='button-icon fa fa-send'></span>
             </button>
           </div>
-          {/* End .col */}
         </div>
       </form>
+
+      {successMessage && (
+        <div className='alert alert-success' role='alert'>
+          {successMessage}
+        </div>
+      )}
 
       {/* End contact */}
     </>
