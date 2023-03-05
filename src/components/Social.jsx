cb8dfe26da76598292aba9ccee0c381d10644aa7
G808 <hucciallstars@gmail.com>
2023-03-05 22:55:52 +0200

    Refactored Social.jsx

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,22 +1,31 @@
 const SocialShare = [
   {
-    iconName: 'fa fa-github',
+    iconName: 'fa-github',
     link: 'https://github.com/Guccifer808/',
   },
-  { iconName: 'fa fa-linkedin', link: 'https://linkedin.com/' },
+  {
+    iconName: 'fa-linkedin',
+    link: 'https://linkedin.com/',
+  },
 ];
 
 const Social = () => {
-  return (
-    <ul className='social list-unstyled pt-1 mb-5'>
-      {SocialShare.map((value, i) => (
-        <li key={i}>
-          <a href={value.link} target='_blank' rel='noreferrer'>
-            <i className={value.iconName}></i>
+  const renderSocialLinks = () => {
+    return SocialShare.map((item, index) => {
+      const { iconName, link } = item;
+      const iconClass = `fa ${iconName}`;
+      return (
+        <li key={index}>
+          <a href={link} target='_blank' rel='noreferrer'>
+            <i className={iconClass}></i>
           </a>
         </li>
-      ))}
-    </ul>
+      );
+    });
+  };
+
+  return (
+    <ul className='social list-unstyled pt-1 mb-5'>{renderSocialLinks()}</ul>
   );
 };
 
